5e688aee29a776c1e4b7c9100c56d2872b9bf379
Berndwl <Wolf@chat.org>
2019-11-03 21:45:53 +0100

    Logging final execution cmd

--- put.sh
+++ put.sh
@@ -7,9 +7,6 @@ echo "Job put started: $(date)"
 if ! [ -x "$(command -v docker)" ]; then
   echo 'Error: docker is not installed.' >&2
   exit 1
-if ! [ -x "$(command -v /usr/local/bin/s3cmd)" ]; then
-  echo 'Error: s3cmd is not installed.' >&2
-  exit 1
 else
   if [[ -n "$MONGODB_USER"  &&  -n "$MONGODB_PASSWORD" &&  -n "$MONGODB_AUTHDB" ]]; then
     EXECUTION_COMMAND="mongodump -u $MONGODB_USER -p $MONGODB_PASSWORD --authenticationDatabase $MONGODB_AUTHDB --archive"
@@ -21,6 +18,8 @@ else
   elif [[ -n "$BACKUP_DB" &&  -z "$BACKUP_COL" ]]; then
         EXECUTION_COMMAND="$EXECUTION_COMMAND --db $BACKUP_DB"
   fi
+
+  echo "Executing $EXECUTION_COMMAND"
 
   docker exec mongo sh -c $EXECUTION_COMMAND > $DATA_PATH
   /usr/local/bin/s3cmd put $PARAMS "$DATA_PATH" "$S3_PATH"
