8c0966974e31de880d29586a70693e6e8c41186c
Berndwl <Wolf@chat.org>
2019-11-03 22:06:32 +0100

    Env var to define container name for mongo, move params to different env to avoid env var overhead

--- put.sh
+++ put.sh
@@ -4,24 +4,19 @@ set -e
 
 echo "Job put started: $(date)"
 
+: ${CONTAINER_NAME:?"CONTAINER_NAME env variable is required"}
+
 if ! [ -x "$(command -v docker)" ]; then
   echo 'Error: docker is not installed.' >&2
   exit 1
 else
-  if [[ -n "$MONGODB_USER"  &&  -n "$MONGODB_PASSWORD" &&  -n "$MONGODB_AUTHDB" ]]; then
-    EXECUTION_COMMAND="mongodump -u $MONGODB_USER -p $MONGODB_PASSWORD --authenticationDatabase $MONGODB_AUTHDB --archive"
-  else
-  	EXECUTION_COMMAND="mongodump --archive"
-  fi
-  if [[ -n "$BACKUP_DB" &&  -n "$BACKUP_COL" ]]; then
-      EXECUTION_COMMAND="$EXECUTION_COMMAND --db $BACKUP_DB --collection $BACKUP_COL"
-  elif [[ -n "$BACKUP_DB" &&  -z "$BACKUP_COL" ]]; then
-        EXECUTION_COMMAND="$EXECUTION_COMMAND --db $BACKUP_DB"
+  if [[ -n "$MONGODB_PARAMS" ]]; then
+    EXECUTION_COMMAND="mongodump --archive $MONGODB_PARAMS"
   fi
 
   echo "Executing $EXECUTION_COMMAND"
 
-  docker exec mongo sh -c $EXECUTION_COMMAND > $DATA_PATH
+  docker exec $CONTAINER_NAME sh -c '$EXECUTION_COMMAND' > $DATA_PATH
   /usr/local/bin/s3cmd put $PARAMS "$DATA_PATH" "$S3_PATH"
 
   echo "Job finished: $(date)"
